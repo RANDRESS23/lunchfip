f7e9013a6e233b5cfb382f2d6a65e913f25b03eb
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-05-17 15:34:02 -0500

    Fixing small details

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -1,7 +1,5 @@
 import axios from 'axios'
 
-const productDomain = process.env.PRODUCTION_DOMAIN_API ?? process.env.PREVIEW_DOMAIN_API
-
 export default axios.create({
-  baseURL: process.env.NODE_ENV === 'development' ? process.env.DEVELOPMENT_DOMAIN_API : productDomain
+  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api' : 'https://lunchfip.vercel.app/api'
 })
