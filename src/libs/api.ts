8a814b10dcad89efb2e65504e09df030f2b37d06
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-05-17 15:10:00 -0500

    Fixing small details

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
+console.log('PROCESS: ->', process.env.NODE_ENV)
+
 export default axios.create({
   baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api' : 'https://lunchfip.vercel.app/api'
 })
