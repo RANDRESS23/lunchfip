3bb03dc1cb07c89908fa02223bba74e77ce2ef0d
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-06-09 23:48:29 -0500

    Fixing small details

--- src/app/api/estudiantes/almuerzos/total/[fecha]/route.ts
+++ src/app/api/estudiantes/almuerzos/total/[fecha]/route.ts
@@ -3,15 +3,21 @@ import { NextResponse } from 'next/server'
 
 export async function GET (_: Request, { params }: { params: { fecha: string } }) {
   try {
-    const fechaAux = new Date(params.fecha)
-    const año = fechaAux.getFullYear()
-    const mes = fechaAux.getMonth() + 1
-    const dia = fechaAux.getDate()
+    const fecha = new Date(params.fecha)
 
-    const fecha = new Date(`${año}-${mes < 10 ? '0' : ''}${mes}-${dia < 10 ? '0' : ''}${dia}`)
-
-    const almuerzos = await db.almuerzos.findUnique({
+    const almuerzosFecha = await db.almuerzos_Fecha.findUnique({
       where: { fecha }
+    })
+
+    if (almuerzosFecha === null) {
+      return NextResponse.json(
+        { message: '¡No hay fecha definida para el servicio de almuerzos!' },
+        { status: 404 }
+      )
+    }
+
+    const almuerzos = await db.almuerzos.findFirst({
+      where: { id_almuerzos_fecha: almuerzosFecha.id_almuerzos_fecha }
     })
 
     if (almuerzos === null) {
