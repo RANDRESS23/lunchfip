623ccc393b61c6904d48a84f74b7f22454674e25
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-08-09 22:13:58 -0500

    Add logo to NotFound page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,22 @@
+import Image from 'next/image'
+
 export default function NotFound () {
   return (
-    <div>not-found</div>
+    <section className='flex justify-center items-center h-screen pt-24'>
+      <Image
+        src='/svgs/logo-lunchfip-dark.svg'
+        alt='logo lunchfip'
+        width={130}
+        height={130}
+        className='hidden dark:flex w-32'
+      />
+      <Image
+        src='/svgs/logo-lunchfip-light.svg'
+        alt='logo lunchfip'
+        width={130}
+        height={130}
+        className='flex dark:hidden w-32'
+      />
+    </section>
   )
 }
