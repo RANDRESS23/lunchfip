60ca20a52f3c92e8612c3559611aa34941d48737
Raúl Quimbaya <raulquimbaya4@gmail.com>
2024-08-10 21:59:50 -0500

    Update NotFound page layout and add 404 message

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function NotFound () {
   return (
-    <section className='flex justify-center items-center h-screen pt-24'>
+    <section className='flex flex-col justify-center items-center h-screen pt-20 gap-5'>
       <Image
         src='/svgs/logo-lunchfip-dark.svg'
         alt='logo lunchfip'
@@ -17,6 +17,11 @@ export default function NotFound () {
         height={130}
         className='flex dark:hidden w-32'
       />
+      <div className='flex justify-center items-center'>
+        <p className='text-center text-2xl dark:text-white'>
+          <span className='font-bold'>404</span> | Página no encontrada
+        </p>
+      </div>
     </section>
   )
 }
